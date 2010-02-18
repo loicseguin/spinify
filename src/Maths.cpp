3d97aaff3ba2e141a09e2c6fa48a2bccfc04eb08
Lo?c S?guin-C. <loicseguin@gmail.com>
2010-02-18 15:09:21 -0500

    Maths.h to contain basic statistics functions. Updated README.

---
+++ src/Maths.cpp
@@ -0,0 +1,28 @@
+/*
+ *  Maths.cpp
+ *  spinify
+ *
+ *  Created by Loïc Séguin-Charbonneau on 10-02-18.
+ *  Copyright 2010 Loïc Séguin-Charbonneau. All rights reserved.
+ *
+ */
+
+#include "Maths.h"
+#include <cmath>
+
+double avg(double* pV, const int nV) {
+	double sum = 0;
+	for (int i = 0; i < nV; i++) {
+		sum += pV[i];
+	}
+	return sum / nV;
+}
+
+double stdDev(double *pV, const int nV) {
+	double avgV = avg(pV, nV);
+	double sum = 0;
+	for (int i = 0; i < nV; i++) {
+		sum += (pV[i] - avgV)*(pV[i] - avgV);
+	}
+	return sqrt(sum / nV);
+}
---
+++ src/Maths.h
@@ -0,0 +1,16 @@
+/*
+ *  Maths.h
+ *  spinify
+ *
+ *  Created by Loïc Séguin-Charbonneau on 10-02-18.
+ *  Copyright 2010 Loïc Séguin-Charbonneau. All rights reserved.
+ *
+ */
+
+#ifndef MATHS_H
+#define MATHS_H
+
+double avg(double* pV, const int nV);
+double stdDev(double *pV, const int nV);
+
+#endif
